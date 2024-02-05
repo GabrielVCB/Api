9ea6b71064ccd3fb3da3b4e075c0b09d7f394768
Gabriel <gabrielvc7@gmail.com>
2024-02-05 14:53:44 -0300

    Atualização

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express"
+import dbConnect from "./config/dbConnect.js";
+import cliente from "./models/Clientes.js";
+
+const connect = await dbConnect()
+
+connect.on("error", (erro) =>{
+    console.error("Erro na conexão!", erro)
+}) 
+
+connect.once("open", () =>{
+    console.log("Sucesso na conexão com o banco de dados")
+}) //aguarda um evento
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json()) //transforma p json antes de mostrar a res
 
-const clientes = [
-    { id: 1, nome: "João"},
-    { id: 2, nome: "Erick"}
-]
+
+// const clientes = [
+//     { id: 1, nome: "João"},
+
+//     { id: 2, nome: "Erick"}
+// ]
+
 
 //Rota Principal
 app.get("/", (req, res) => {
@@ -15,41 +30,51 @@ app.get("/", (req, res) => {
 })
 
 //Consultar todos os clientes
-app.get("/clientes", (req, res) => {
-    res.status(200).json(clientes);
+app.get("/clientes", async (req, res) => {
+    const listaClientes = await cliente.find({}) // estrutura vazia ({}) retorna tudo
+    res.status(200).json(listaClientes);
 })
-
 
 //Consultar o cliente pelo id
 app.get("/clientes/:id", (req, res) => {
-    const index = buscarCliente(req.params.id);
-    res.status(200).json(clientes[index])
+    const index = buscarCliente(req.params.id); // (req.params.id) traz o parametro id na requisicao
+    res.status(200).json(clientes[index]) // clientes é array, index recebeu o retorno da busca
 })
-
 
 //Cadastrar novo cliente
 app.post("/clientes", (req, res) => {
-    clientes.push(req.body);  
-    res.status(201).send("Cliente cadastrado com sucesso!")
+    clientes.push(req.body);  //adiciona na ultima posicao
+    res.status(201).send("Cliente cadastrado com sucesso!") //status 201 indica confirmacao que foi criado com sucesso
 })
 
 //Atualizar o cadastro de um cliente
 app.put("/clientes/:id", (req,res) => {
     const index = buscarCliente(req.params.id);
-    clientes[index].nome = req.body.nome;
-    res.status(200).json(clientes)
+    clientes[index].nome = req.body.nome; //acessar o nome na requisacao na parte "body"
+    res.status(200).json(clientes) //exibe todos clientes
+
 })
+//nome atualizar, no PUT > body (json) > {"nome": "novo nome"}
+
 
 //Excluir um cliente
-app.delete("/clientes/:id", (req, res) => {
+app.delete("/clientes/:id", (req,res) => {
     const index = buscarCliente(req.params.id);
-    clientes.splice(index, 1);
-    res.status(200).send("Cadastro removido com sucesso!")
+    clientes.splice(index, 1); //excluir posicao e quantas casas
+    res.status(200).send("Cadastro removido com sucesso!") 
+
 })
 
-function buscarCliente(id){
-    var index = clientes.findIndex(cliente => cliente.id === Number(id))
-    return index
-}
+// function buscarCliente(id){
+//     var index = clientes.findIndex(cliente => cliente.id === Number(id)) //number(id) converte o id em número
+//     return index
+// }
+
+// function buscarCliente(id){
+//     clientes.findIndex(parametro => {cliente.id === i}}) faz o loop no id do cliente ate o id do cliente for igual ao id desejado 
+// } 
+//find = retorna valor e findIndex = retorna a posicão
+
+
 
 export default app;
